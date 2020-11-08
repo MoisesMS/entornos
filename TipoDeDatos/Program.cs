766c17dbe7bf868a1715d57dc3353e32688fbba0
Moisés Morente <moisesmorentesalazar@gmail.com>
2020-11-08 11:57:33 +0100

    Añadido ejercicio 'Tipos de daatos'. Init Commit

---
+++ TipoDeDatos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TipoDeDatos {
+    
+    class Program {
+    
+        static void Main(string[] args) {
+
+            byte   v1 = 200;
+            Console.WriteLine(v1);
+            
+            sbyte  v2 = -100;
+            Console.WriteLine(v2);
+            
+            short  v3 = 30000;
+            Console.WriteLine(v3);
+            
+            ushort v4 = 60000;
+            Console.WriteLine(v4);
+            
+            int    v5 = -2000000000;
+            Console.WriteLine(v5);
+            
+            uint   v6 = 2000000000;
+            Console.WriteLine(v6);
+            
+            long   v7 = -1000000000000000000;
+            Console.WriteLine(v7);
+            
+            ulong  v8 = 1000000000000000000;
+            Console.WriteLine(v8);
+
+            Console . ReadKey ();
+
+        }
+    }
+}
