6ffbb740d1404874b5e1c71817de502fe0f419cb
= <moisesmorentes@gmail.com>
2020-11-09 11:34:21 +0100

    Añadido ejercicio 12

---
+++ Tarea 12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tarea12{
+    
+    class Program {
+    
+        static void Main(string[] args) {
+            // Programa que pida una fecha y compruebe si coincide con la fecha de navidad
+            // Formato de fecha: dd/mm/aaaa o dd-mm-aaaa
+
+            String fecha = pedirDato("Introduce una fecha");
+            int datos = {};
+
+            datos = fecha.Split("/");
+
+            Console.WriteLine(datos);
+
+            DateTime fecha = new DateTime()
+
+            
+        }
+
+
+
+
+
+        static void pedirDato(string msg) {
+            Console.Write(msg);
+            retrun Console.ReadLine();
+        }
+    }
+}
---
+++ Tarea12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tarea12 {
+	class Program {
+		static void Main(string[] args) {
+			string line = pedirdato("Introduce una fecha: dd/mm/aaaa");
+		
+		}
+	
+	
+	
+	
+	
+		static string pedirdato(string msg) {
+			Console.Write(msg);
+			return Console.ReadLine();
+		}
+	}
+}
